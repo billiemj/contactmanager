082c9ac50b2bf4558b9edc9a5cc296109f007f56
Billie Muzzy <billiemj@gmail.com>
2019-04-06 21:58:52 -0500

    Events In React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {};
+  onShowClick = () => {
+    console.log(this.state);
+  };
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3 ">
         <ul className="list=group list-group-flush ">
-          <h4 className="card-title"> {name}</h4>
+          <h4 className="card-title">
+            {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          </h4>
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
         </ul>
